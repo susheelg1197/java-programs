946636959930effd368dc0b31e1b9c2fdbba4d7c
susheel.g <susheel.g@peopleinteractive.in>
2021-12-14 08:45:47 +0530

    Dec 13 Java Tuts

---
+++ BASICS/HelloWorld.java
@@ -0,0 +1,19 @@
+
+// Rules while defining the class name 
+/*
+    1. The file name of java and the class name muse be same
+    2. Type javac filename to compile this code.
+    3. Type java filename to run your program.
+    4. Java is case sensitive that means 'Hello' and 'hello' has different meanings
+    5. All method names must start with a lowercase.
+    6. Class Names first letter must be capital.
+    7. public static void main(String []args) is mandate in every java program .
+
+
+*/
+
+public class HelloWorld{
+    public static void main(String []args){
+        System.out.println("Hello Java!!!");
+    }
+}
